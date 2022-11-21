fd70ddf30eebebce4a5dbab0edc06974225a8d03
Kaiyun (Kiki) Kang <89110858+kiki0515@users.noreply.github.com>
2022-11-21 09:56:17 -0500

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -99,7 +99,7 @@ class ResNet(nn.Module):
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+    return ResNet(BasicBlock, [3, 3, 3, 2])
 
 if __name__ == "__main__":
     net = ResNet18()
