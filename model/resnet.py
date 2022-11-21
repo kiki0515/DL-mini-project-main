5f84b6346270128cbfef44498938fa91254bf684
Kaiyun (Kiki) Kang <89110858+kiki0515@users.noreply.github.com>
2022-11-21 14:19:03 -0500

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -9,10 +9,10 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            in_planes, planes, kernel_size=4, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=4,
                                stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
@@ -68,7 +68,7 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=4,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
@@ -100,7 +100,7 @@ class ResNet(nn.Module):
 
 
 def ResNet18():
-    return ResNet(Bottleneck, [2, 2, 2, 1])
+    return ResNet(BasicBlock, [2, 2, 2, 1])
 
 if __name__ == "__main__":
     net = ResNet18()
