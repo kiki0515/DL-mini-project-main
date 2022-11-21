653f13a3fa1117d9175968c74a369c8394fcc63f
Kaiyun (Kiki) Kang <89110858+kiki0515@users.noreply.github.com>
2022-11-21 14:41:18 -0500

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -68,8 +68,8 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=5,
+                               stride=1, padding=2, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -100,7 +100,7 @@ class ResNet(nn.Module):
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [2, 3, 2, 1])
+    return ResNet(BasicBlock, [2, 2, 2, 1])
 
 if __name__ == "__main__":
     net = ResNet18()
