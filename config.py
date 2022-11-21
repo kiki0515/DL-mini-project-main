2a8b67bf6d05dc47fbb9772fed482a765e09613e
Kaiyun (Kiki) Kang <89110858+kiki0515@users.noreply.github.com>
2022-11-21 09:17:59 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+def get_configs():
+    train_params = {
+        "device": "cuda",
+        "max_epoch": 100,
+        "output_dir": "/outputs/allAugs_AdamW_lr0.01_WeightDecay5e-4_batchSize128"
+    }
+
+    dataset_params = {
+        "train_bs": 128,
+        "test_bs": 256,
+        #"test_bs": 128,
+    }
+
+    model_params = {
+
+    }
+
+    callback_params = {
+        "patience": 5,
+        "save_final_model": True,
+    }
+
+    optimizer_params = {
+        "lr": 0.01,
+        "type": "AdamW",
+        "kwargs": {
+            "weight_decay": 5e-4,
+            "amsgrad": True
+        }
+    }
+
+    return train_params, dataset_params, callback_params, optimizer_params
---
+++ train.py
@@ -0,0 +1,59 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import numpy as np
+import random
+
+from model.resnet import ResNet18
+from trainer import Trainer
+from config import get_configs
+
+
+def get_train_test_trans():
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.RandomRotation(5)
+    ])
+
+    return transform_train, transform_test
+
+
+if __name__ == "__main__":
+    seed = 6953
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    # get configs
+    train_params, dataset_params, callback_params, optimizer_params = get_configs()
+
+    # get model
+    model = ResNet18()
+
+    # get data augmentations
+    transform_train, transform_test = get_train_test_trans()
+
+    # get trainer
+    trainer = Trainer(
+        model, 
+        dataset_params,
+        train_params,
+        callback_params,
+        optimizer_params,
+        transform_train,
+        transform_test
+    )
+    trainer.train()
