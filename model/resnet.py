eaf0752291717f08faf1e32e745ac2195e5a07b3
Kaiyun (Kiki) Kang <89110858+kiki0515@users.noreply.github.com>
2022-11-22 00:27:13 -0500

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -9,7 +9,7 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=5, stride=stride, padding=2, bias=False)
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=5,
                                stride=1, padding=2, bias=False)
@@ -67,8 +67,8 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=5,
+                               stride=1, padding=2, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
