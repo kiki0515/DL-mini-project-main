a56a1909d84dd2c6067cf4c1cfd6c8c737c4e90d
Kaiyun (Kiki) Kang <89110858+kiki0515@users.noreply.github.com>
2022-11-22 00:22:00 -0500

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -9,10 +9,10 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=5, stride=stride, padding=1, bias=False)
+            in_planes, planes, kernel_size=5, stride=stride, padding=2, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=5,
-                               stride=1, padding=1, bias=False)
+                               stride=1, padding=2, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
@@ -38,11 +38,11 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=5,
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion *
-                               planes, kernel_size=5, bias=False)
+                               planes, kernel_size=3, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
@@ -68,7 +68,7 @@ class ResNet(nn.Module):
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=5,
-                               stride=1, padding=1, bias=False)
+                               stride=1, padding=2, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -99,7 +99,7 @@ class ResNet(nn.Module):
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [3, 5, 3, 3])
+    return ResNet(BasicBlock, [4, 5, 3, 3])
 
 if __name__ == "__main__":
     net = ResNet18()
